2ce2ac1f88d01b6dc8ac85b38c041583a0bb1960
BextiyarNovruzov <baxtiyarbn-bp215@code.edu.az>
2024-10-18 17:47:57 +0400

    Pataxt Salati oyunumu teqdim edirem...

---
+++ PaytaxtSalati/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace PaytaxtSalati
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            //Step: Bütün məlumatları doldurmadan obyekt yaratmaq olmasın.
+			int Secimler;
+			do
+			{
+				Console.WriteLine("Paytaxt Salati oyununa xos gelmisiniz.Oyunumuz ");
+
+				Console.WriteLine("0 - İnformasiya almaq üçün\n 1 - Shoot metodu üçün\n 2 - GetRemainBulletCount metodu üçün\n  3 - Reload metodu üçün\n 4 - ChangeFireMode metodu üçün\n 5 - Proqramdan dayandırmaq üçün");
+				Secimler = int.Parse(Console.ReadLine());
+				Weapon new_Weapon = null;
+				switch (Secimler)
+				{
+					case 0:
+						Console.WriteLine("Tutum Daxil edin");
+						int gulletutumu = int.Parse(Console.ReadLine());
+						Console.WriteLine("Gulle sayi Daxil edin");
+						int gullesayi = int.Parse(Console.ReadLine());
+						Console.WriteLine("Firemode Daxil edin. tekli ve avtomatik olaraq sec!");
+						string atismode = Console.ReadLine();
+						new_Weapon = new Weapon(gulletutumu, gullesayi, atismode);
+						break;
+					case 1:
+						new_Weapon.Shoot();
+						break;
+					case 2:
+						Console.WriteLine($"Daragin tam dolmasi ucun lazim olan gulle sayi: {new_Weapon.GetRemainBulletCount(new_Weapon.Gulletutumu,new_Weapon.Gullesayi)}");
+						break;
+					case 3:
+					
+						 new_Weapon.Reload();
+						Console.WriteLine("Daragi relod edildi");
+						break;
+					case 4:
+						new_Weapon.ChangeFireMode();
+                        Console.WriteLine("Atis modu deyisdirildi");
+						break;
+					case 5:
+                        Console.WriteLine("Game over");
+						break;
+					default:
+						Console.WriteLine("Invalid option");
+						break ;
+
+
+				}
+
+					
+			}
+			while (Secimler != 5);
+		}
+	}
+}
---
+++ PaytaxtSalati/Weapon.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaytaxtSalati
+{
+	internal class Weapon
+	{
+
+		//Step: Fields yaratmaq
+		private int _gulletutumu;
+		private int _gullesayi;
+		private string _atismodu;
+
+		//Step: Construkto yaratmaq 
+		public Weapon(int daragingulletutumu, int daragdakigullesayi, string atismodu)
+		{
+			_gulletutumu = daragingulletutumu;
+			_gullesayi = daragdakigullesayi;
+			_atismodu = atismodu;
+		}
+
+		//Step:Propertyes yaratmaq
+		public int Gulletutumu
+		{
+			get
+			{
+				return _gulletutumu;
+			}
+			set 
+			{
+				if( value < 0)
+				{
+					Console.WriteLine("Menfi sayda gulle sayi olmaz");
+				}
+				else
+				{
+					value = _gulletutumu;
+
+				}
+			}
+		}
+		public int Gullesayi 
+		{
+			get
+			{
+				return _gullesayi;
+			}
+			set
+			{
+				if (value < 0)
+				{
+					Console.WriteLine("Menfi sayda gulle sayi olmaz");
+				}
+				else
+				{
+					value = _gullesayi;
+
+				}
+			}
+		}
+		public string Atismodu { get; set; }
+
+		//Step: Shoot methodu yaratmaq
+		public void Shoot()
+		{
+			if (Gullesayi <= 0)
+			{
+				Console.WriteLine("Daraq bosdur.");
+				return;
+			}
+
+			if (Atismodu== "tekli")
+			{
+				TekliShot();
+			}
+			else if (Atismodu == "avtomatik")
+			{
+				AutomatikShot();
+			}
+		}
+		//tekli atis
+		private void TekliShot()
+		{
+			if (Gullesayi > 0)
+			{
+				Gullesayi--;
+				Console.WriteLine("Bir gulle atıldı. Qalan gulle sayı: " + Gullesayi);
+			}
+			else
+			{
+				Console.WriteLine("Daraq bosdur.");
+			}
+		}
+
+		// Avtomatik atıs
+		private void AutomatikShot()
+		{
+			Gullesayi = 0;
+		}
+
+		//Step: GetRemainBulletCount() - darağın tam dolması üçün lazım olan güllə sayını qaytarır.
+		public int GetRemainBulletCount(int Gulletutumu, int Gullesayi)
+		{
+			int lazimolan = Gulletutumu - Gullesayi;
+			return lazimolan;
+		}
+
+
+		//Step: Reload() - darağı yenidən doldurur.
+
+		public int Reload()
+		{
+			Gulletutumu = Gullesayi;
+			return Gulletutumu;
+		}
+
+		//Step: ChangeFireMode() - Atış modunu dəyişir.
+		public string ChangeFireMode()
+		{
+			if (Atismodu == "tekli")
+			{
+				Atismodu = "avtomatik";
+				return Atismodu;
+			}
+			else
+			{
+                Console.WriteLine("atis modenu duzgun secin ya tekli yada  avtomatik sece billersiz.");
+			}
+			return Atismodu;
+		}
+
+
+	}
+}
